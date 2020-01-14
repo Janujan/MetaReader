ae56e5a79fb2863eb2ea63658e1933877b841a54
Janujan <janujan.selva@gmail.com>
2020-01-13 19:44:39 -0500

    form creation

---
+++ metareaderapp/src/app/form/form.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Metadata } from '../metadata';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent{
+
+  constructor() { }
+
+  model = new Metadata(1, 'user1', 'csv');
+  submitted = false;
+
+   // TODO: Remove this when we're done
+   get diagnostic() { return JSON.stringify(this.model); }
+
+}
---
+++ metareaderapp/src/app/metadata.ts
@@ -0,0 +1,8 @@
+export class Metadata {
+    constructor(
+        public id: number,
+        public name: string,
+        public filetype: string,
+        public fields?: string,
+      ) {  }
+}
